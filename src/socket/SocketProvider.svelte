2594fb92b70733da25b4403e40f5afdb1b35625d
Jamie Harding <jharding142@icloud.com>
2025-09-30 10:56:59 +0200

    Update SocketProvider.svelte

--- src/socket/SocketProvider.svelte
+++ src/socket/SocketProvider.svelte
@@ -1,18 +1,25 @@
 <script lang="ts">
 	import { setSocket } from './context';
 	import { Socket } from './Socket';
-
+	
 	interface Props {
 		children?: import('svelte').Snippet;
 		url?: string;
 	}
+	
+	let { children, url = 'ws://localhost:9980/' }: Props = $props();
+	
+	let socket: Socket | null = $state(null);
+	
+	$effect(() => {
+		socket = new Socket(url);
+		socket.connect();
+		setSocket(socket);
 
-	let { children, url }: Props = $props();
-
-	const socket = new Socket(url);
-	socket.connect();
-
-	setSocket(socket);
+		return () => {
+			socket?.disconnect();
+		};
+	});
 </script>
 
 {@render children?.()}
